cfb58b2751e261b194e6c81407865f48bb1ad490
sumantapakira <sumantapakira@gmail.com>
2022-05-13 17:49:36 +0200

    Update MechanicalService.java

--- src/MechanicalService.java
+++ src/MechanicalService.java
@@ -3,5 +3,7 @@ package carwash;
 public interface MechanicalService {
 	
 	public int calculatePrice(Car car);
+	public void setServiceCode(String code);
+	public void setServiceDescription(String description);
 
 }
